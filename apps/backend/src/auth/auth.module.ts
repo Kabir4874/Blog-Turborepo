78b773eef09288139e5eea9c1e40a1ee98f08f2c
kabir4874 <kabir.cse.bd@gmail.com>
2025-03-24 20:18:56 +0600

    auth

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRE_IN'),
+        },
+      }),
+    }),
+  ],
   providers: [AuthResolver, AuthService],
 })
 export class AuthModule {}
--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,11 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
 import { verify } from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SignInInput } from './dto/signIn.input';
+import { AuthJwtPayload } from './types/auth-jwtPayload';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateLocalUser({ email, password }: SignInInput) {
     const user = await this.prisma.user.findUnique({
@@ -21,7 +27,19 @@ export class AuthService {
     return user;
   }
 
-  async generateToken(userId:number){
-    
+  async generateToken(userId: number) {
+    const payload: AuthJwtPayload = { sub: userId };
+    const accessToken = await this.jwtService.signAsync(payload);
+    return { accessToken };
+  }
+
+  async login(user: User) {
+    const { accessToken } = await this.generateToken(user.id);
+    return {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar,
+      accessToken,
+    };
   }
 }
